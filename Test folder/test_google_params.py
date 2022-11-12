c8c6877d46cb7a7345927113e6e8bec00e0a1de1
Tuba <tuba@talent.com>
2022-11-12 18:54:42 +0530

    update with import service

--- Test folder/test_google_params.py
+++ Test folder/test_google_params.py
@@ -1,12 +1,14 @@
 import pytest
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+
 
 
 @pytest.fixture(params=["chrome"], scope="class")
 def init_driver(request):
     if request.param == "chrome":
-       web_driver = webdriver.Chrome(ChromeDriverManager().install())
+       web_driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
     request.cls.driver= web_driver
     yield
     web_driver.close()
